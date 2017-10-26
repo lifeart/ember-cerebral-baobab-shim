a947564cfcce6c6aee55b37559dd9cd6a0be6f58
lifeart <lifeart92@gmail.com>
2017-10-26 04:25:00 +0300

    cerebral-baobab-shim

--- vendor/shims/cerebral-baobab.js
+++ vendor/shims/cerebral-baobab.js
@@ -2,11 +2,7 @@
   /* globals define, self */
   function vendorModule(BaseModel) {
     'use strict';
-
-    return {
-      'default': BaseModel,
-      __esModule: true,
-    };
+    return BaseModel;
   }
   // export an object with a default property that contains the 'hljs' global.
   define('cerebral/lib/BaseModel', ['cerebral/BaseModel'], vendorModule);
